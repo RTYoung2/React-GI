3b37585a78ae29fe2ee317854f44a46fb28e9ec3
Robert Young <robertyoung@MacBook-Pro-3.local>
2021-01-21 00:07:26 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PersonSingle from "./Component/PersonSingle";
+import PersonObject from "./Component/PersonObject";
+// dont forget the from
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: "James Intile",
+      email: "1212@gmail.com",
+      number: "11111111",
+
+      Robert: {
+        fullName: "lil Rob",
+        email: "ryoung@gmail.com",
+        number: "1812",
+        food: "Burgers And chicken",
+        car: "Porshe",
+        game: "Escape From Tarkov",
+      },
+    };
+
+    // const robert = {
+    //   name: 'Robert',
+    //   age: "22",
+    //   number: "777-777-7777",
+    //   email:"12345@gmail.com",
+    // }
+
+    // this.state = {
+    //   robert
+    // }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Person basicInfo = {this.state.personA}/>
+<Person basicInfo = {this.state.personB}/> */}
+        {/* <Basicinfo name={kiera} /> */}
+        {/* {JSON.stringify(this.state.robert)} */}
+        {/* {JSON.stringify(this.state.person)} */}
+        <PersonSingle
+          fullName="Robert Young"
+          email="12345@gmail.com"
+          number="777777777"
+        />
+        <PersonSingle
+          fullName={this.state.fullName}
+          email={this.state.email}
+          number={this.state.number}
+        />
+        <PersonObject person={this.state.Robert} />
+      </div>
+    );
+  }
 }
 
 export default App;
